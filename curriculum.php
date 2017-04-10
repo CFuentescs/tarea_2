78d9a8fead711d7c3549604b3d2655fc784dc0ef
carlos_fuentes <c.fuentescorts@gmail.com>
2017-04-09 22:35:41 -0300

    archivo de html5 tarea2

---
+++ curriculum.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+	<title>Carlos Fuentes</title>
+
+</head>
+<body>
+
+	<h1>Curriculum Vitae</h1>
+
+	<IMG SRC="carlos.jpg" WIDTH=178 HEIGHT=180 ALT="de este guapeton">
+
+	<h2>Resumen profesional</h2>
+
+	<FONT SIZE="2">Ingeniero de computación e informatica,en cuarto año de su carrera, pro-activo, manejo de ingles basico, con un rapido aprendizaje,facilidad en las relaciones publicas y manejo de distintos lenguajes de computación</FONT>
+
+	<h2>Datos personales</h2>
+
+
+	 <table >
+    <tr>
+      	<td><font size="3"> Nombre: </font></td> <td><font size="2"> Carlos Fuentes Cortes</font></td>
+    </tr>
+
+    
+    <tr>
+      	<td><font size="3"> Rut: </font></td> <td><font size="2"> 11.222.333-4</font></td>
+  	 </tr>
+    <tr>
+      	<td><font size="3"> Celular: </font></td> <td><font size="2"> +5699988774455</font></td>
+    </tr>
+
+    <tr>
+      	<td><font size="3"> Email: </font></td> <td><font size="2">ejemplo@ejemplo.cl</font></td>
+    </tr>
+
+ <tr>
+      	<td><font size="3"> Nacionalidad: </font></td> <td><font size="2">Chileno</font></td>
+    </tr>
+
+  <tr>
+      	<td><font size="3"> Fecha de nacimiento: </font></td> <td><font size="2">24 de septiembre del 1994</font></td>
+    </tr>
+     
+
+   <tr>
+      	<td><font size="3"> Email: </font></td> <td><font size="2">ejemplo@ejemplo.cl</font></td>
+    </tr>
+</table>
+
+<h2>Antecedentes Academicos</h2>
+			
+	 <table >
+    <tr>
+      	<td><font size="3"> Enseñansa basica:  </font></td> <td><font size="2"> -2012 Colegio de grace school</font></td>
+    </tr>
+
+    
+    <tr>
+      	<td><font size="3"> Enseñanza superior: </font></td> <td><font size="2"> 2012- ?? Universidad Andres bello</font></td>
+  	 
+    
+    </tr>
+
+    </table>
+
+</body>
+</html>
